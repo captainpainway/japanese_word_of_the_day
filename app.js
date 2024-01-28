367ddcf8c87e83b2083ce5b7a818a079eb68b91a
Mary Knize <maryknize@gmail.com>
2024-01-27 22:03:05 -0500

    Updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-require('dotenv').config();
+require('dotenv').config({path: ".env"});
 const fs = require("fs");
 
 const {
@@ -41,8 +41,13 @@ async function run() {
     },
   ];
 
+  let past_words = '';
+  if (fs.existsSync(`${__dirname}/past_words.txt`)) {
+    past_words = fs.readFileSync(`${__dirname}/past_words.txt`, 'utf8').trim().replaceAll('\n', ', ');
+  }
+
   const parts = [
-    {text: "Provide one random elementary-level Japanese word in the following format:\n{\n\"hiragana\": word,\n\"katakana\": word,\n\"kanji\": word,\n\"romaji\": romaji,\n\"pronunciation\": pronunciation,\n\"definition\": definition,\n\"part_of_speech\": part of speech of the word,\n\"sentence\": provide an example sentence using the word,\n\"sentence_romaji\" : provide a romaji version of the example sentence,\n\"sentence_translation\": translate the example sentence into English\n}\nDo not return the following words: []"},
+    {text: "Provide one random elementary-level Japanese word in the following format:\n{\n\"hiragana\": word,\n\"katakana\": word,\n\"kanji\": word,\n\"romaji\": romaji,\n\"pronunciation\": pronunciation,\n\"definition\": definition,\n\"part_of_speech\": part of speech of the word,\n\"sentence\": provide an example sentence using the word,\n\"sentence_romaji\" : provide a romaji version of the example sentence,\n\"sentence_translation\": translate the example sentence into English\n}\nDo not return the following words: ["+past_words+"]"},
   ];
 
   const result = await model.generateContent({
@@ -55,50 +60,57 @@ async function run() {
   return response.text();
 }
 
-function createIndexPage(date) {
-  let template = fs.readFileSync(`__dirname/template.html`, 'utf8');
-  let styles = fs.readFileSync(`__dirname/style.css`, 'utf8');
+// Create index page, add data to template, save index.html.
+function createIndexPage(last) {
+  let template = fs.readFileSync(`${__dirname}/template.html`, 'utf8');
+  let styles = fs.readFileSync(`${__dirname}/style.css`, 'utf8');
   run().then((data) => {
     const word = JSON.parse(data);
     for (let key of Object.keys(word)) {
       template = template.replace(`{{${key}}}`, word[key]);
     }
     template = template.replace('{{css}}', styles);
-    let previousPage = `${__dirname}/pages/${getPastDate(1)}.html`;
-    if (fs.existsSync(previousPage)) {
-      template = template.replace('<a id="back"><</a>', `<a id="back" href="${previousPage}"><</a>`);
+    if (fs.existsSync(`${__dirname}/pages/${last}.html`)) {
+      template = template.replace('<a id="back"><</a>', `<a id="back" href="${process.env.OUT_PATH}/pages/${last}.html"><</a>`);
     }
-    fs.writeFileSync('./index.html', template);
+    fs.writeFileSync(`${__dirname}/index.html`, template);
+    fs.appendFileSync(`${__dirname}/past_words.txt`, `${word['kanji']}\n`);
   });
 }
 
-function getPastDate(numDays) {
-  const date = new Date();
-  date.setDate(date.getDate() - numDays);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${year}${month}${day}`;
-}
-
-function updateAndCopy() {
-  let template = fs.readFileSync(`${__dirname}/template.html`, 'utf8');
-  let previousPage = `${__dirname}/pages/${getPastDate(2)}.html`;
-  if (fs.existsSync(previousPage)) {
-    template = template.replace('<a id="forward">></a>', `<a id="forward" href="${__dirname}/index.html">></a>`);
+function updateAndCopy(last, currentPage, curr, second = false) {
+  let template = fs.readFileSync(currentPage, 'utf8');
+  if (second) {
+    template = template.replace(`<a id="forward" href="${process.env.OUT_PATH}/index.html">></a>`, `<a id="forward" href="${curr}">></a>`);
+  } else {
+    template = template.replace('<a id="forward">></a>', `<a id="forward" href="${curr}">></a>`);
   }
-  fs.copyFile('./index.html', `./pages/${getPastDate(1)}.html`, (err) => {
+  fs.writeFileSync(currentPage, template);
+  fs.copyFile(currentPage, `${__dirname}/pages/${last}.html`, (err) => {
     if (err) throw err;
   });
 }
 
-function updateYesterday() {
-
-}
-
 function copyIndexToPages() {
-  updateAndCopy()
-  createIndexPage();
+  // If past words have been recorded, process older files.
+  if (fs.existsSync(`${__dirname}/past_words.txt`)) {
+    let past_words = fs.readFileSync(`${__dirname}/past_words.txt`, 'utf8').trim().split('\n');
+    let last = past_words.pop();
+    if (last) {
+      // Update index.html to point to index.html and rename it to the word.html.
+      updateAndCopy(last, `${__dirname}/index.html`, `${process.env.OUT_PATH}/index.html`);
+    }
+    let second = past_words.pop();
+    if (second) {
+      // Update the most recent word file to point at the renamed file that was the index.
+      updateAndCopy(second, `${__dirname}/pages/${second}.html`, `${process.env.OUT_PATH}/pages/${last}.html`, true);
+    }
+    // Create new index page.
+    createIndexPage(last);
+  } else {
+    // Create initial index page.
+    createIndexPage(null);
+  }
 }
 
 copyIndexToPages();
