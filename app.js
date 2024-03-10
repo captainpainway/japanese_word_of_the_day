aac3e8c5ec0bcd0820facaadca709525792b6388
Mary Knize <maryknize@gmail.com>
2024-03-09 20:36:57 -0500

    Update model in app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const {
   HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const MODEL_NAME = "gemini-pro";
+const MODEL_NAME = "gemini-1.0-pro";
 const API_KEY = process.env.API_KEY;
 
 async function run() {
